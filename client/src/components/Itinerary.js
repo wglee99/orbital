95685935a78e8b8aff4625b1918710cd7503e277
wglee99 <littlereddot40@gmail.com>
2021-07-26 09:41:27 +0800

    fixed drag and drop

--- client/src/components/Itinerary.js
+++ client/src/components/Itinerary.js
@@ -6,23 +6,115 @@ import { getCart } from "../redux/itemRedux";
 
 const Itinerary = () => {
   const { user, isAuthenticated } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
+  const dispatcher = useDispatch();
 
   useEffect(() => {
     if (isAuthenticated) {
-      dispatch(getCart(user._id));
+      dispatcher(getCart(user._id));
     }
-  }, [dispatch, user, isAuthenticated]);
+  }, [dispatcher, user, isAuthenticated]);
 
   const item = useSelector((state) => state.item);
 
-  console.log(item);
 
   const { items } = item;
 
-  console.log(items);
 
   const [listItems, setListItems] = useState(items?.items);
+  // const [selected, setSelected] = useState([]);
+
+  // a little function to help us with reordering the result
+  // const reorder = (list, startIndex, endIndex) => {
+  //   const result = Array.from(list);
+  //   const [removed] = result.splice(startIndex, 1);
+  //   result.splice(endIndex, 0, removed);
+
+  //   return result;
+  // };
+
+  /**
+   * Moves an item from one list to another list.
+   */
+  // const move = (source, destination, droppableSource, droppableDestination) => {
+  //   const sourceClone = Array.from(source);
+  //   const destClone = Array.from(destination);
+  //   const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  //   destClone.splice(droppableDestination.index, 0, removed);
+
+  //   const result = {};
+  //   result[droppableSource.droppableId] = sourceClone;
+  //   result[droppableDestination.droppableId] = destClone;
+
+  //   return result;
+  // };
+
+  // const grid = 8;
+
+  // const getItemStyle = (isDragging, draggableStyle) => ({
+  //   // some basic styles to make the items look a bit nicer
+  //   userSelect: "none",
+  //   padding: grid * 2,
+  //   margin: `0 0 ${grid}px 0`,
+
+  //   // change background colour if dragging
+  //   background: isDragging ? "lightgreen" : "white",
+
+  //   // styles we need to apply on draggables
+  //   ...draggableStyle,
+  // });
+
+  // const getListStyle = (isDraggingOver) => ({
+  //   background: isDraggingOver ? "lightblue" : "lightgrey",
+  //   padding: grid,
+  //   width: 250,
+  // });
+
+  // const id2List = {
+  //   droppable: listItems,
+  //   droppable2: selected,
+  // };
+
+  // const getList = (id) => id2List[id];
+
+  // const onDragEnd = (result) => {
+  //   const { source, destination } = result;
+
+  //   // dropped outside the list
+  //   if (!destination) {
+  //     return;
+  //   }
+
+  //   if (source.droppableId === destination.droppableId) {
+  //     const items = reorder(
+  //       getList(source.droppableId),
+  //       source.index,
+  //       destination.index
+  //     );
+
+  //     if (source.droppableId === id2List.droppable2) {
+  //       setSelected(items);
+  //     } else {
+  //       const result = move(
+  //         getList(source.droppableId),
+  //         getList(destination.droppableId),
+  //         source,
+  //         destination
+  //       );
+
+  //       setListItems(result.droppable);
+  //       setSelected(result.droppable2);
+        
+        // this.setState({
+        //     items: result.droppable,
+        //     selected: result.droppable2
+    //   }
+    // }
+
+// console.log(selected);
+// console.log(listItems);
+
+  // };
 
   function handleOnDragEnd(result) {
     if (!result.destination) return;
@@ -45,8 +137,8 @@ const Itinerary = () => {
                 {listItems.map((item, index) => {
                   return (
                     <Draggable
-                      key={item.id}
-                      draggableId={item.id}
+                      key={item.productId}
+                      draggableId={item.productId}
                       index={index}
                     >
                       {(provided) => (
@@ -72,6 +164,63 @@ const Itinerary = () => {
         <h3>Itinerary</h3>
       </Container>
     </div>
+
+    // <DragDropContext onDragEnd={onDragEnd}>
+    //   <Droppable droppableId="droppable">
+    //     {(provided, snapshot) => (
+    //       <div
+    //         ref={provided.innerRef}
+    //         style={getListStyle(snapshot.isDraggingOver)}
+    //       >
+    //         {listItems.map((item, index) => (
+    //           <Draggable key={item.productId} draggableId={item.productId} index={index}>
+    //             {(provided, snapshot) => (
+    //               <div
+    //                 ref={provided.innerRef}
+    //                 {...provided.draggableProps}
+    //                 {...provided.dragHandleProps}
+    //                 style={getItemStyle(
+    //                   snapshot.isDragging,
+    //                   provided.draggableProps.style
+    //                 )}
+    //               >
+    //                 {item.name}
+    //               </div>
+    //             )}
+    //           </Draggable>
+    //         ))}
+    //         {provided.placeholder}
+    //       </div>
+    //     )}
+    //   </Droppable>
+    //   <Droppable droppableId="droppable2">
+    //     {(provided, snapshot) => (
+    //       <ul
+    //         ref={provided.innerRef}
+    //         style={getListStyle(snapshot.isDraggingOver)}
+    //       >
+    //         {selected.map((item, index) => (
+    //           <Draggable key={item.productId} draggableId={item.productId} index={index}>
+    //             {(provided, snapshot) => (
+    //               <ul
+    //                 ref={provided.innerRef}
+    //                 {...provided.draggableProps}
+    //                 {...provided.dragHandleProps}
+    //                 style={getItemStyle(
+    //                   snapshot.isDragging,
+    //                   provided.draggableProps.style
+    //                 )}
+    //               >
+    //                 {item.name}
+    //               </ul>
+    //             )}
+    //           </Draggable>
+    //         ))}
+    //         {provided.placeholder}
+    //       </ul>
+    //     )}
+    //   </Droppable>
+    // </DragDropContext>
   ) : (
     <h1 style={{ textAlign: "center" }}>
       <strong>You are not logged in!</strong>

4fb530193050b1ea23c8108d0bf94e351b9244ba
wglee99 <littlereddot40@gmail.com>
2021-07-26 17:02:02 +0800

    fixed drag and drop

--- client/src/components/Itinerary.js
+++ client/src/components/Itinerary.js
@@ -20,55 +20,190 @@ const Itinerary = () => {
 
   const [listItems, setListItems] = useState(items?.items);
 
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
-
-    const items = Array.from(listItems);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setListItems(items);
+  const columnsFromBackend = {
+    [0]: {
+      name: 'My List',
+      items: listItems
+    },
+    [1]: {
+      name: 'Itinerary',
+      items: []
+    }
   }
 
-  return isAuthenticated ? (
-    <div style={{ display: "flex" }}>
-      <Container border="1px" border-radius="2px">
-        <h3>My List</h3>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="listItems">
-            {(provided) => (
-              <ul ref={provided.innerRef} {...provided.droppableProps}>
-                {listItems.map((item, index) => {
-                  return (
-                    <Draggable
-                      key={item.productId}
-                      key={item.productId}
-                      draggableId={item.productId}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <ul
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <p>{item.name}</p>
-                        </ul>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </Container>
+  const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+  
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
+        }
+      });
 
-      <Container>
-        <h3>Itinerary</h3>
-      </Container>
-    </div>
+      console.log(columns);
+     
+
+
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+
+
+
+  };
+
+  const [columns, setColumns] = useState(columnsFromBackend);
+
+  //reordering items within list
+  // const onDragEnd = (result) => {
+  //   if (!result.destination) return;
+
+  //   const items = Array.from(listItems);
+  //   const [reorderedItem] = items.splice(result.source.index, 1);
+  //   items.splice(result.destination.index, 0, reorderedItem);
+
+  //   setListItems(items);
+  // }
+
+  return isAuthenticated ? (
+
+    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+    <DragDropContext
+      onDragEnd={result => onDragEnd(result, columns, setColumns)}
+    >
+      {Object.entries(columns).map(([columnId, column], index) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center"
+            }}
+            key={columnId}
+          >
+            <h2>{column.name}</h2>
+            <div style={{ margin: 8 }}>
+              <Droppable droppableId={columnId} key={columnId}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? "lightblue"
+                          : "lightgrey",
+                        padding: 4,
+                        width: 250,
+                        minHeight: 500
+                      }}
+                    >
+                      {column.items.map((item, index) => {
+                        return (
+                          <Draggable
+                            key={item.productId}
+                            draggableId={item.productId}
+                            index={index}
+                          >
+                            {(provided, snapshot) => {
+                              return (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  style={{
+                                    userSelect: "none",
+                                    padding: 16,
+                                    margin: "0 0 8px 0",
+                                    minHeight: "50px",
+                                    backgroundColor: snapshot.isDragging
+                                      ? "#263B4A"
+                                      : "#456C86",
+                                    color: "white",
+                                    ...provided.draggableProps.style
+                                  }}
+                                >
+                                  {item.name}
+                                </div>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
+          </div>
+        );
+      })}
+    </DragDropContext>
+  </div>
+
+
+    // <div style={{ display: "flex" }}>
+    //   <Container border="1px" border-radius="2px">
+    //     <h3>My List</h3>
+    //     <DragDropContext onDragEnd={handleOnDragEnd}>
+    //       <Droppable droppableId="listItems">
+    //         {(provided) => (
+    //           <ul ref={provided.innerRef} {...provided.droppableProps}>
+    //             {listItems.map((item, index) => {
+    //               return (
+    //                 <Draggable
+    //                   key={item.productId}
+    //                   draggableId={item.productId}
+    //                   index={index}
+    //                 >
+    //                   {(provided) => (
+    //                     <ul
+    //                       ref={provided.innerRef}
+    //                       {...provided.draggableProps}
+    //                       {...provided.dragHandleProps}
+    //                     >
+    //                       <p>{item.name}</p>
+    //                     </ul>
+    //                   )}
+    //                 </Draggable>
+    //               );
+    //             })}
+    //             {provided.placeholder}
+    //           </ul>
+    //         )}
+    //       </Droppable>
+    //     </DragDropContext>
+    //   </Container>
+
+    //   <Container>
+    //     <h3>Itinerary</h3>
+    //   </Container>
+    // </div>
   ) : (
     <h1 style={{ textAlign: "center" }}>
       <strong>You are not logged in!</strong>
